480c71ca99fd06b934c69bcd6f077b90c6506b8d
Wander D <wd.delossantos@gmail.com>
2025-07-28 22:13:29 -0400

    implement art section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { ScrollTrigger, SplitText } from "gsap/all"
 import gsap from "gsap"
 import Navbar from "./components/Navbar"
-import Hero from "./components/Hero.jsx"
-import Cocktails from "./components/Cocktails.jsx"
-import About from "./components/About.jsx"
+import Hero from "./components/Hero"
+import Cocktails from "./components/Cocktails"
+import About from "./components/About"
+import Art from "./components/Art"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -14,6 +15,8 @@ const App = () => {
     <Hero />
     <Cocktails />
     <About />
+    <Art />
+    <div className="h-[100vh]"></div>
   </main>
   )
 }
